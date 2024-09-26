81f4e35e0384ab810eaf25ab1ee480be9cb0bf3a
Elsa Hamon <elsa.hamon@etu.unicaen.fr>
2024-09-26 17:05:47 +0200

    correction of the issues of the timer. add of transition in display depending of the status

--- scripts/script.js
+++ scripts/script.js
@@ -5,45 +5,102 @@ let reset = document.getElementById("reset");
 let travail = document.getElementById("travail");
 let repos = document.getElementById("repos");
 
+let minWork = 25;
+let secWork = 0;
+let minPause = 5;
+let secPause = 0;
+let running = null;
+let isWorking = false;
+
 travail.classList.add("disabled");
 repos.classList.add("disabled");
 
-timer.textContent = "25:00";
+timerPlaying = false;
 
-// function runTimer() {
-//     let minute = timer.textContent.substring(0, 2);
-//     let seconde = timer.textContent.substring(3,2);
+function displayTimer(minute, seconde){
+    let minD = (minute < 10 ? '0' + minute : minute);
+    let secD = (seconde < 10 ? '0' + seconde : seconde);
+    timer.textContent = minD + ':' + secD;
+}
 
-//     while(minute>0 && seconde>0){
-//         if(seconde==0){
-//             minute = minute - 1;
-//             seconde = 59;
-//         }
-//         else{
-//             seconde = seconde - 1;
-//         }
-//         timer.textContent = minute + ':' + seconde;
-//     }
-// }
+
+function enableWorkButton(){
+    travail.classList.remove("disabled");
+    travail.classList.remove("btn-outline-warning");
+    travail.classList.add("btn-warning");
+}
+
+function enablePauseButton(){
+    repos.classList.remove("disabled");
+    repos.classList.add("btn-info");
+    repos.classList.remove("btn-outline-info");
+}
+
+function disableWorkButton(){
+    travail.classList.add("disabled");
+    travail.classList.remove("btn-warning");
+    travail.classList.add("btn-outline-warning");
+}
+
+function disablePauseButton(){
+    repos.classList.add("disabled");
+    repos.classList.remove("btn-info");
+    repos.classList.add("btn-outline-info");
+}
+
+function runTimer() {
+    let minP = minWork;
+    let secP = secWork;
+    timerPlaying = true;
+    isWorking = true;
+    running = setInterval(function() {
+        if(secP==0 & minP !=0){
+            minP--;
+            secP = 59;
+        }
+        else{
+            secP--;
+        }
+        if (minP==0 && secP==0) {
+            if(isWorking){
+                isWorking = false;
+                minP = minPause;
+                secP = secPause;
+                document.body.classList.add("pause");
+                timer.classList.add("pause");
+                disableWorkButton();
+                enablePauseButton();
+            }
+            else{
+                isWorking = true;
+                minP = minWork;
+                secP = secWork;
+                document.body.classList.remove("pause");
+                timer.classList.remove("pause");
+                enableWorkButton();
+                disablePauseButton();
+            }
+        }
+        displayTimer(minP, secP);
+    }, 1000);
+}
+
+
+document.onloadstart = displayTimer(minWork, secWork);
 
 start.onclick = function(){
-    let iconElement;
     if(timerPlaying == false){
         start.innerHTML = 'Reset <i class="bi bi-arrow-counterclockwise"></i>';
-        travail.classList.remove("disabled");
-        travail.classList.remove("btn-outline-warning");
-        travail.classList.add("btn-warning");
-        repos.classList.add("disabled");
-        // runTimer();
+        disablePauseButton();
+        enableWorkButton();
+        runTimer();
     }
     else {
+        clearInterval(running);
+        displayTimer(minWork, secWork);
         start.innerHTML = 'Start <i class="bi bi-caret-right-fill"></i>';
-        travail.classList.add("disabled");
-        travail.classList.remove("btn-warning");
-        travail.classList.add("btn-outline-warning");
-        repos.classList.add("disabled");
-        repos.classList.remove("btn-info");
-        repos.classList.add("btn-outline-info");
+        disablePauseButton();
+        disableWorkButton();
+        timerPlaying = false;
     }
-    start.appendChild(iconElement);
 }
