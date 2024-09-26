e793f2e5d561babb6ad06de3b69a4984ff7ea97e
Elsa Hamon <elsa.hamon@etu.unicaen.fr>
2024-09-26 15:35:11 +0200

    add a wip for the running timmer function

--- scripts/script.js
+++ scripts/script.js
@@ -8,23 +8,36 @@ let repos = document.getElementById("repos");
 travail.classList.add("disabled");
 repos.classList.add("disabled");
 
-let timerPlaying = false;
-
 timer.textContent = "25:00";
+
+// function runTimer() {
+//     let minute = timer.textContent.substring(0, 2);
+//     let seconde = timer.textContent.substring(3,2);
+
+//     while(minute>0 && seconde>0){
+//         if(seconde==0){
+//             minute = minute - 1;
+//             seconde = 59;
+//         }
+//         else{
+//             seconde = seconde - 1;
+//         }
+//         timer.textContent = minute + ':' + seconde;
+//     }
+// }
 
 start.onclick = function(){
     let iconElement;
     if(timerPlaying == false){
         start.innerHTML = 'Reset <i class="bi bi-arrow-counterclockwise"></i>';
-        timerPlaying = true;
         travail.classList.remove("disabled");
         travail.classList.remove("btn-outline-warning");
         travail.classList.add("btn-warning");
         repos.classList.add("disabled");
+        // runTimer();
     }
     else {
         start.innerHTML = 'Start <i class="bi bi-caret-right-fill"></i>';
-        timerPlaying = false;
         travail.classList.add("disabled");
         travail.classList.remove("btn-warning");
         travail.classList.add("btn-outline-warning");
